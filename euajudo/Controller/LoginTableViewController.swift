e009541cae4a69d4d4fe818eb9013cf75de64745
filipealva <ofilipealvarenga@gmail.com>
2015-04-11 19:39:35 -0300

    Add login with email

---
+++ euajudo/Controller/LoginTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginTableViewController.swift
+//  euajudo
+//
+//  Created by Filipe Alvarenga on 11/04/15.
+//  Copyright (c) 2015 euajudo. All rights reserved.
+//
+
+import Cocoa
+
+class LoginTableViewController: UITableViewController {
+
+}
