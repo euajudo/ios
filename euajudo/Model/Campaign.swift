81185bb2185f64a62b1ac3ad9d94aeeaf7fa0d6f
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2015-04-11 12:52:40 -0300

    Create Campaing model

---
+++ euajudo/Model/Campaign.swift
@@ -0,0 +1,28 @@
+//
+//  Campaign.swift
+//  euajudo
+//
+//  Created by Rafael Kellermann Streit on 4/11/15.
+//  Copyright (c) 2015 euajudo. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+    Campaing model.
+
+    @param name Campaign name.
+    @param description Campaing description.
+    @param createdAt Campaing creation date.
+    @param mainMedia Dictionary with main media values (type, url and videoUrl if is a video)
+ */
+
+class Campaign {
+    
+    var name: String!
+    var description: String!
+    var createdAt: NSDate!
+    var mainMedia: [String: AnyObject]!
+
+}
