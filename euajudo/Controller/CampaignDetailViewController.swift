d6443f91d82b0b79212d35e4ceed0de8c2261618
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2015-04-11 16:27:47 -0300

    Create Details controller

---
+++ euajudo/Controller/CampaignDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CampaignDetailViewController.swift
+//  euajudo
+//
+//  Created by Rafael Kellermann Streit on 4/11/15.
+//  Copyright (c) 2015 euajudo. All rights reserved.
+//
+
+import UIKit
+
+
+class CampaignDetailViewController: UIViewController {
+    
+    var campaign: Campaign?
+
+}
