aaece49415b42fb3e067f5b7ba4addf937939411
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2015-04-12 05:43:40 -0300

    Create references to TextFields in controller

---
+++ euajudo/Controller/PaymentInformationsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PaymentInformationsViewController.swift
+//  euajudo
+//
+//  Created by Rafael Kellermann Streit on 4/12/15.
+//  Copyright (c) 2015 euajudo. All rights reserved.
+//
+
+import UIKit
+
+
+class PaymentInformationsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var buttonPayment: UIButton!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldNumber: UITextField!
+    @IBOutlet weak var textFieldExpireDate: UITextField!
+    @IBOutlet weak var textFieldSecurity: UITextField!
+    
+
+    // MARK: - IBAction
+    
+    @IBAction func buttonPaymentPressed(sender: AnyObject) {
+        buttonPayment.setTitle("", forState: .Normal)
+        activityIndicatorView.startAnimating()
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
